e238c6e87a1701841fcbd74ab9a29a5df6d10303
Achmadirfani <ahmadirfani@gmail.com>
2021-06-10 07:40:02 +0700

    p

---
+++ retrieve.php
@@ -0,0 +1,26 @@
+<?php 
+  // require 'connection.php';  
+
+	$conn = mysqli_connect("localhost","root","","android_db");
+	
+	$result = array();
+	$result['data'] = array();
+	$select= "SELECT *from data";
+	$responce = mysqli_query($conn,$select);
+	
+	while($row = mysqli_fetch_array($responce))
+		{
+			$index['id']      = $row['0'];
+			$index['name']    = $row['1'];
+			$index['email']   = $row['2'];
+			$index['contact'] = $row['3'];
+			$index['address'] = $row['4'];
+			
+			array_push($result['data'], $index);
+		}
+			
+			$result["success"]="1";
+			echo json_encode($result);
+			mysqli_close($conn);
+
+ ?>
